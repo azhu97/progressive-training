d922a40f2ab156112bcdbf5d185528a0a0afbbc5
Alan Zhu <alanzhu9000@gmail.com>
2025-07-07 21:49:50 -0400

    updated folder structure

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -1,25 +1,14 @@
-let workouts = [];
+let workouts = []; // Temporary in-memory storage
 
 const getWorkouts = (req, res) => {
-    res.json(workouts);
-}
+  res.json(workouts);
+};
 
 const addWorkout = (req, res) => {
-    const { exercise, weight, reps, sets, date } = req.body;
-    const newWorkout = {
-        idd: Date.now(),
-        exercise,
-        weight,
-        reps, 
-        sets, 
-        date
-    };
-    workouts.push(newWorkout);
-    res.status(201).json(newWorkout);
-}
+  const { exercise, weight, reps, sets, date } = req.body;
+  const newWorkout = { id: Date.now(), exercise, weight, reps, sets, date };
+  workouts.push(newWorkout);
+  res.status(201).json(newWorkout);
+};
 
-module.exports = {
-    getWorkouts,
-    addWorkout
-}
-
+module.exports = { getWorkouts, addWorkout };
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const workoutRoutes = require("./routes/workoutRoutes")
+const workoutRoutes = require("./routes/workoutRoutes");
 
 app.use(cors());
 app.use(express.json());
@@ -9,6 +9,4 @@ app.use(express.json());
 app.use("/api/workouts", workoutRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
