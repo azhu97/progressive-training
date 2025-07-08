cfc3d8038b21f4f2f8637cbe664287482055ce73
Alan Zhu <alanzhu9000@gmail.com>
2025-07-07 21:58:42 -0400

    middleware folder

--- server/server.js
+++ server/server.js
@@ -8,5 +8,9 @@ app.use(express.json());
 
 app.use("/api/workouts", workoutRoutes);
 
+require("dotenv").config();
+const connectDB = require("./config/db");
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
